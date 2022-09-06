0122e6c085fcbb368145f09fde138fcf414a93a4
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-09-06 10:25:21 +0600

    feat: add current js link

--- constants.py
+++ constants.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class EdupageLinks(Enum):
     main_db_link = 'https://fmalmnis.edupage.org/rpr/server/maindbi.js?__func=mainDBIAccessor'
+    current_js_link = 'https://fmalmnis.edupage.org/timetable/server/currenttt.js?__func=curentttGetData'
 
 
 class EdupageMainDbIDs(Enum):
