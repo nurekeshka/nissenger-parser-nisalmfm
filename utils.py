f86a90f06fb3e0f8352758f8186a5c4ccc10be05
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-09-06 10:54:28 +0600

    feat: add gettings current week

--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
+from datetime import timedelta
 from datetime import date
+from typing import Tuple
 from config import YEAR
 
 
 def get_current_year() -> int:
     return YEAR if YEAR is not None else date.today().year
+
+
+def get_current_week() -> Tuple[str]:
+    today: date = date.today()
+    weekday: int = today.weekday()
+
+    firstday: date = today - timedelta(days=weekday)
+    lastday: date = firstday + timedelta(days=6)
+
+    return tuple( map( lambda day: day.strftime('%Y-%m-%d'), ( firstday, lastday ) ) )
