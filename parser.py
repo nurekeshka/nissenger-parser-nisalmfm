029f79f8d5125caa3f1811be827b3c58aae21731
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-09-07 12:36:15 +0600

    feat: add typing to dict list

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ from typing import List
 
 def convert_main_db_response(json: dict) -> List[dict]:
     tables: List[dict] = json['r']['tables']
-    data: list = list()
+    data: List[dict] = list()
 
     for table in tables:
         df = parse_json_to_dictionary(table)
@@ -20,8 +20,8 @@ def parse_json_to_dictionary(data: dict) -> dict:
         if len(row) == 2:
             id, value = row.values()
             dataset[id] = value
-        elif len(row) == 3:
-            id, starttime, endtime = row.values()
+        elif len(row) == 4:
+            id, _, starttime, endtime = row.values()
             dataset[id] = (starttime, endtime)
         else:
             raise ValueError(f'{row=}')
