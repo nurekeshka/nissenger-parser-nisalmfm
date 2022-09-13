71af221107a7dcd67ed89d68c02f55671a6e730d
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-09-13 12:24:14 +0600

    feat: add parser links

---
+++ constants.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Links(Enum):
+    database = 'https://fmalmnis.edupage.org/rpr/server/maindbi.js?__func=mainDBIAccessor'
+    lessons = 'https://fmalmnis.edupage.org/timetable/server/currenttt.js?__func=curentttGetData'
