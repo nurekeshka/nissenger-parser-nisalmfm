92a90f6ebb4da0701fb9d7e1190e357c859bab68
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-09-13 13:30:31 +0600

    feat: add indexes

--- constants.py
+++ constants.py
@@ -4,3 +4,11 @@ from enum import Enum
 class Links(Enum):
     database = 'https://fmalmnis.edupage.org/rpr/server/maindbi.js?__func=mainDBIAccessor'
     lessons = 'https://fmalmnis.edupage.org/timetable/server/currenttt.js?__func=curentttGetData'
+
+
+class Indexes(Enum):
+    teachers = 0
+    subjects = 1
+    classrooms = 2
+    classes = 3
+    periods = 4
