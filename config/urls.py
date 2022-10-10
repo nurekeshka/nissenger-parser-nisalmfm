d0bcd2c0b5118d59e8f136dc000f118ec7098a43
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-10-10 14:56:52 +0600

    feat: add fmalmnis urls

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fmalmnis/', include('fmalmnis.urls'))
+    path('fmalmnis/', include('apps.fmalmnis.urls'))
 ]
