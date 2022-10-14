f2876416decf269b46dd88c8def63cf56f04f46f
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-10-14 08:40:03 +0600

    feat: add loading entities and filling database

--- apps/fmalmnis/utils.py
+++ apps/fmalmnis/utils.py
@@ -1,13 +1,76 @@
-from .constants import TIMETABLE_DATABASE_LINK
-from .constants import TIMETABLE_LESSONS_LINK
 from datetime import date
 from typing import List
+
 import requests
 
+from apps.core import models
+from apps.fmalmnis import constants as const
+from apps.fmalmnis.constants import TimetableIndexes as indexes
 
-def load_main_db() -> requests.Response:
+
+def load_teachers(teachers: list, timetable: models.Timetable):
+    change = const.TEACHER_CHANGES
+    changes = const.TEACHER_CHANGES.keys()
+
+    for teacher in teachers:
+        name = change[teacher['short']
+                      ] if teacher['short'] in changes else teacher['short']
+        models.Teacher.objects.create(
+            name=name.lower().title(), timetable=timetable)
+
+
+def load_subjects(subjects: list, timetable: models.Timetable):
+    change = const.SUBJECT_CHANGES
+    changes = const.SUBJECT_CHANGES.keys()
+
+    for subject in subjects:
+        name = change[subject['name']
+                      ] if subject['name'] in changes else subject['name']
+        models.Subject.objects.create(name=name, timetable=timetable)
+
+
+def load_offices(classrooms: list, timetable: models.Timetable):
+    change = const.CLASSROOM_CHANGES
+    changes = const.CLASSROOM_CHANGES.keys()
+
+    for classroom in classrooms:
+        name = change[classroom['short']
+                      ] if classroom['short'] in changes else classroom['short']
+        models.Classroom.objects.create(name=name, timetable=timetable)
+
+
+def load_periods(periods: list, timetable: models.Timetable):
+    for period in periods:
+        models.Period.objects.create(
+            number=period['period'], starttime=period['starttime'], endtime=period['endtime'], timetable=timetable)
+
+
+def load_classes(class_names: list, timetable: models.Timetable):
+    for class_name in class_names:
+        grade = int(class_name['short'][:-1])
+        letter = class_name['short'][-1]
+
+        entity = models.Class.objects.create(
+            grade=grade, letter=letter, timetable=timetable)
+
+        for number in range(1, 3):
+            group = models.Group.objects.create(
+                name=f'{number} - группа: {entity}', timetable=timetable)
+            group.classes.add(entity)
+
+
+def load_entities(tables: dict, timetable: models.Timetable) -> dict:
+    load_teachers(get_data_rows(tables[indexes.teachers.value]), timetable)
+    load_subjects(get_data_rows(tables[indexes.subjects.value]), timetable)
+    load_offices(get_data_rows(tables[indexes.classrooms.value]), timetable)
+    load_periods(get_data_rows(tables[indexes.periods.value]), timetable)
+    load_classes(get_data_rows(tables[indexes.classes.value]), timetable)
+
+
+def load_main_db(timetable: models.Timetable) -> requests.Response:
     data = request_timetable_database()
     tables = get_tables(data)
+    load_entities(tables, timetable)
 
 
 def get_tables(data: dict) -> List[dict]:
@@ -19,7 +82,7 @@ def get_data_rows(data: dict) -> List[dict]:
 
 
 def request_timetable_database():
-    return requests.post(url=TIMETABLE_DATABASE_LINK, json=_timetable_database_data()).json()
+    return requests.post(url=const.TIMETABLE_DATABASE_LINK, json=_timetable_database_data()).json()
 
 
 def _timetable_database_data():
