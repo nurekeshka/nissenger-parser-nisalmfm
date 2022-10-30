73860470fa5ab5cef77445239c5a3c2bbb18ce87
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-10-30 20:34:41 +0600

    fix: change name of entities file

---
+++ entities.py
@@ -0,0 +1,47 @@
+
+
+class BaseEntity(object):
+    def __init__(self, id: str, name: str):
+        self.id: str = id
+        self.name: str = name
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(BaseEntity):
+    pass
+
+
+class Subject(BaseEntity):
+    pass
+
+
+class Classroom(BaseEntity):
+    pass
+
+
+class Class(BaseEntity):
+    def __init__(self, id: str, name: str):
+        self.id: str = id
+        self.grade: int = int(name[:-1])
+        self.letter: int = name[-1]
+
+    def __str__(self):
+        return f'{self.grade}{self.letter}'
+
+
+class Period(BaseEntity):
+    def __init__(self, number: int, starttime: str, endtime: str):
+        self.number: int = int(number)
+        self.starttime: str = starttime
+        self.endtime: str = endtime
+
+    def __str__(self):
+        return f'{self.starttime} - {self.endtime}'
+
+
+class Day(BaseEntity):
+    def __init__(self, number: int, name: str):
+        self.number = int(number)
+        self.name = name
