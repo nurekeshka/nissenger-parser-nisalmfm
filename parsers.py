50b295ce3a58eb914bb297db8abdaebe4426faa1
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-01 23:53:29 +0600

    feat: add lessons parser

--- parsers.py
+++ parsers.py
@@ -1,3 +1,4 @@
+from typing import List
 import settings
 import entities
 import datetime
@@ -7,7 +8,7 @@ class AbstractParser(object):
     fixes = None
 
     @classmethod
-    def format(self, data: dict) -> str:
+    def format(self, data: dict) -> object:
         kwargs = self.fix(data)
         return self.to_object(**kwargs)
 
@@ -34,7 +35,7 @@ class DictionaryFixesParser(AbstractParser):
         return {'id': id, 'name': name}
 
     @classmethod
-    def to_object(self, id: str, name: str):
+    def to_object(self, id: str, name: str) -> entity:
         return self.entity(id=id, name=name)
 
 
@@ -67,7 +68,7 @@ class ClassesParser(AbstractParser):
     name = 'short'
 
     @classmethod
-    def fix(self, data: dict):
+    def fix(self, data: dict) -> dict:
         id = data['id']
         grade = int(data[self.name][:-1])
         letter = data[self.name][-1]
@@ -75,7 +76,7 @@ class ClassesParser(AbstractParser):
         return {'id': id, 'grade': grade, 'letter': letter}
 
     @classmethod
-    def to_object(self, id: str, grade: int, letter: str):
+    def to_object(self, id: str, grade: int, letter: str) -> entities.Class:
         return entities.Class(id=id, grade=grade, letter=letter)
 
 
@@ -86,12 +87,12 @@ class PeriodParser(AbstractParser):
     endtime = 'endtime'
 
     @classmethod
-    def fix(self, data: dict):
+    def fix(self, data: dict) -> dict:
         data[self.period] = int(data[self.period])
         return data
 
     @classmethod
-    def to_object(self, id: str, period: int, starttime: str, endtime: str):
+    def to_object(self, id: str, period: int, starttime: str, endtime: str) -> entities.Period:
         return entities.Period(id=id, number=period,
                                starttime=starttime, endtime=endtime)
 
@@ -100,7 +101,7 @@ class DaysParser(AbstractParser):
     date_format = '%Y-%m-%d'
 
     @classmethod
-    def fix(self, data: str):
+    def fix(self, data: str) -> dict:
         day = datetime.datetime.strptime(data, self.date_format)
         number = day.isoweekday()
         name = settings.DAYS[number]
@@ -108,5 +109,67 @@ class DaysParser(AbstractParser):
         return {'number': number, 'name': name}
 
     @classmethod
-    def to_object(self, number: int, name: str):
+    def to_object(self, number: int, name: str) -> entities.Day:
         return entities.Day(number=number, name=name)
+
+
+class LessonsParser(AbstractParser):
+    # Returns: { 'lessons': [{'subject': entities.Subject, 'teacher': entities.Teacher, ...}, {...}}, {...}] }
+    @classmethod
+    def format(self, data: dict) -> entities.Lesson:
+        kwargs = data['lesson']
+        kwargs['database'] = data['database']
+
+        lessons = self.fix(**kwargs)
+        return self.to_object(lessons)
+
+    @classmethod
+    def fix(self, database,
+            date: str, uniperiod: str,
+            starttime: str, endtime: str,
+            subjectid: str, classids: List[str],
+            groupnames: List[str], teacherids: List[str],
+            classroomids: List[str], igroupid: str = None,
+            type: str = None, durationperiods: int = 1,
+            cellSlices: str = None, cellOrder: int = None):
+        lessons = list()
+
+        subject = database['subjects'][f'id={subjectid}']
+
+        teachers = [database['teachers']
+                    [f'id={teacherid}'] for teacherid in teacherids]
+
+        classrooms = [database['classrooms']
+                      [f'id={classroomid}'] for classroomid in classroomids]
+
+        day = DaysParser().format(date)
+
+        group = entities.Group(name=groupnames[0], classes=[
+                               database['classes'][f'id={classid}'] for classid in classids])
+
+        periods = [database['periods'][f'number={x}'] for x in range(
+            int(uniperiod), int(uniperiod) + durationperiods)]
+
+        for period in periods:
+            lesson = dict()
+
+            lesson['subject'] = subject
+            lesson['teacher'] = teachers[0]
+            lesson['classroom'] = classrooms[0] if len(
+                classrooms) == 1 else ''
+            lesson['group'] = group
+            lesson['period'] = period
+            lesson['day'] = day
+
+            lessons.append(lesson)
+
+        return lessons
+
+    @ classmethod
+    def to_object(self, lessons: List[dict]) -> List[entities.Lesson]:
+        return [entities.Lesson(subject=lesson['subject'],
+                                teacher=lesson['teacher'],
+                                classroom=lesson['classroom'],
+                                group=lesson['group'],
+                                period=lesson['period'],
+                                day=lesson['day']) for lesson in lessons]
