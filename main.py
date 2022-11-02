dd0167ddcd06d9aabe6d5c69a800aa6018664262
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-02 18:05:58 +0600

    feat: add main file

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from database import Database
 from sender import FmalmNisEdupageSender as Sender
 from models import Timetable
 from parser import FmalmNisEdupageDatabaseParser as Parser
-'''
 
 
 def main():
-    '''
     payload = Sender.request_database()
     database = Database.load(payload=payload, parser=DatabaseParser)
 
@@ -18,11 +16,48 @@ def main():
         data = Sender.request_lessons(type=By.SUBJECT, payload=subject.id)
 
         for lesson_json in data:
-            lesson = parser.json_to_object(lesson_json)
+            lesson = parser.format(lesson_json)
             timetable.add(lesson)
 
     timetable.export()
-    '''
+'''
+
+import senders
+import constants
+import databases
+import parsers
+import entities
+import requests
+
+
+def bootstrap():
+    sender = senders.DatabaseSender()
+    sender.request()
+
+
+def main():
+    sender = senders.DatabaseSender()
+    sender.request()
+
+    database = databases.TimetableDatabase()
+    database.timetable_load(sender.data)
+
+    timetable = entities.Timetable()
+
+    for subject in database['subjects']:
+        sender = senders.LessonsSender()
+        sender.request(id=subject.id, by=constants.By.SUBJECT)
+
+        for lesson__json in sender.data:
+            parser = parsers.LessonsParser()
+            lessons = parser.format(
+                data={'lesson': lesson__json, 'database': database})
+            timetable.add(lessons)
+
+    data = timetable.export()
+
+    sender = senders.TimetableSender()
+    sender.request(timetable=data)
 
 
 if __name__ == '__main__':
