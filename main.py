72ac12fc52216e201e2d01a16173c8dfdec3d6c4
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-11-03 22:30:20 +0600

    fix: remove draft of main function

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-'''
-from database import Database
-from sender import FmalmNisEdupageSender as Sender
-from models import Timetable
-from parser import FmalmNisEdupageDatabaseParser as Parser
-
-
-def main():
-    payload = Sender.request_database()
-    database = Database.load(payload=payload, parser=DatabaseParser)
-
-    timetable = Timetable()
-    parser = LessonsParser()
-
-    for subject in database.subjects:
-        data = Sender.request_lessons(type=By.SUBJECT, payload=subject.id)
-
-        for lesson_json in data:
-            lesson = parser.format(lesson_json)
-            timetable.add(lesson)
-
-    timetable.export()
-'''
-
 import senders
 import constants
 import databases
