666fcec78957f3f1686dde8d65c241fd9a4c53a8
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-12-01 10:42:08 +0600

    feat: add basic methodist error

--- exceptions.py
+++ exceptions.py
@@ -4,3 +4,10 @@ class TableTypeError(TypeError):
     def __init__(self):
         super(TableTypeError, self).__init__(
             'AbstractDatabase.load() argument of method must be dictionary that contains str: AbstractTable')
+
+
+class BaseMethodistError(Exception):
+    details = 'Methodist fucked up... again...'
+
+    def __init__(self):
+        super(BaseMethodistError, self).__init__(self.details)
