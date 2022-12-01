4e0542afb584aae82b45732f7a5b4ff58551515a
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-12-01 11:27:38 +0600

    feat: add nothing found exception

--- exceptions.py
+++ exceptions.py
@@ -1,17 +1,23 @@
 
 
-class TableTypeError(TypeError):
+class BasicException(BaseException):
+    details = 'Something went wrong'
+
     def __init__(self):
-        super(TableTypeError, self).__init__(
-            'AbstractDatabase.load() argument of method must be dictionary that contains str: AbstractTable')
+        super(BasicException, self).__init__(self.details)
 
 
-class BaseMethodistError(Exception):
+class TableTypeError(BaseException):
+    details = 'AbstractDatabase.load() argument of method must be dictionary that contains str: AbstractTable'
+
+
+class BaseMethodistError(BaseException):
     details = 'Methodist fucked up... again...'
-
-    def __init__(self):
-        super(BaseMethodistError, self).__init__(self.details)
 
 
 class MoreThanOneResult(BaseMethodistError):
     details = 'Holy shit... Maybe methodist created several teachers with the same name again?'
+
+
+class NothingFound(BaseException):
+    details = 'Database didn\'t find anything'
