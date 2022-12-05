9e300b5dd11a154d63b81313a1c458d9c3b78109
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-12-05 21:59:19 +0600

    fix: send vie local explicit telegram bot

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import requests
 import senders
 
 
+bot = senders.bot
 app = Flask(__name__)
 
 
@@ -22,16 +23,24 @@ def launch_parser():
 
         response = main()
     except requests.exceptions.ConnectionError:
+        bot.send_to_admins(text='[FAIL] API is not accessible')
         return responses.APINotAccessible()
     except Exception as exception:
+        bot.send_to_admins(
+            message=f'[FAIL] Parsing failed because of the: {exception.__class__.__name__}')
         return responses.ReportError(exception)
 
     if response:
         if response.status_code == status.HTTP_201_CREATED:
+            bot.send_to_admins(
+                message='[OK] Timetable was successfully updated')
             return responses.SuccessfullyUpdated()
         else:
+            bot.send_to_admins(
+                message=f'[FAIL] API responded with status code: {response.status_code}')
             return responses.APIError()
     else:
+        bot.send_to_admins(text='[OK] Timetable was not changed')
         return responses.TimetableDidNotChange()
 
 
